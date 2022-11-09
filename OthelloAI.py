a955184ed8dcec6723792b78d89b1421cd4f5421
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2022-11-08 23:46:59 -0600

    Update OthelloAI.py

--- OthelloAI.py
+++ OthelloAI.py
@@ -1,9 +1,28 @@
+""" 
+Ty Pederson
+10349042
+11/6/22
+CSC 475 001
+Fall 2022
+
+Assignment 3
+An Intelligent Othello Player
+
+Othello game that can be played against either another person
+or against an AI which uses mini-max with alpha-beta pruning
+ """
+
+
 import tkinter as tk
 import math
 
 window = tk.Tk()
 
 canvas = tk.Canvas(window, bg="green", height = 800, width=800)
+score = tk.Label(window, width=50, relief="raised", pady=10, font="Times 20 italic bold")
+victor = tk.Label(window, width=50, relief="raised", pady=10, font="Times 20 italic bold")
+score.pack(side="top")
+
 
 x = 0
 y = 0
@@ -37,6 +56,9 @@ cpu = "white"
 turn = user
 cpuOn = True
 
+
+
+
 def placePiece(x, y, color):
     canvas.create_oval(x * 100 + 10, y * 100 + 10, x * 100 + 90, y * 100 + 90, fill=color)
 
@@ -56,8 +78,42 @@ def drawBoard():
         print(f"{boardArr[x][0]},{boardArr[x][1]},{boardArr[x][2]},{boardArr[x][3]},{boardArr[x][4]},{boardArr[x][5]},{boardArr[x][6]},{boardArr[x][7]}")
     print()
 
+    blackScore, whiteScore = getScore()
+    myText = f"Black: {blackScore}\t\t\tWhite: {whiteScore}"
+    score.config(text = myText)
 
-def checkPos(x, y):
+
+def getScore():
+    blackScore = 0
+    whiteScore = 0
+    for x in range(8):
+        for y in range(8):
+            match boardArr[y][x]:
+                case 0:
+                    continue
+                case 1:
+                    blackScore += 1
+                    continue
+                case 2:
+                    whiteScore += 1
+                    continue
+    return blackScore, whiteScore
+    
+
+def displayVictor():
+    black, white = getScore()
+    if (black > white):
+        myText = "Black Wins!"
+    elif (white > black):
+        myText = "White Wins!"
+    else:
+        myText = "Tie!"
+
+    victor.config(text = myText)
+    victor.pack(side="top")
+
+
+def checkPos(x, y, board):
 
     valToCheck = 1
     currVal = 2
@@ -67,7 +123,7 @@ def checkPos(x, y):
 
     
 
-    if (boardArr[y][x] != 0):
+    if (board[y][x] != 0):
         return False
 
     for xDir, yDir in [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]:
@@ -75,10 +131,10 @@ def checkPos(x, y):
         newX += xDir
         newY += yDir
         
-        if (newX > 7 or newX < 0 or newY > 7 or newY < 0 or boardArr[newY][newX] == currVal):
+        if (newX > 7 or newX < 0 or newY > 7 or newY < 0 or board[newY][newX] == currVal):
             continue
 
-        while (boardArr[newY][newX] == valToCheck):
+        while (board[newY][newX] == valToCheck):
             newX += xDir
             newY += yDir
 
@@ -87,12 +143,13 @@ def checkPos(x, y):
                 newY -= yDir
                 break
 
-        if (boardArr[newY][newX] == currVal):
+        if (board[newY][newX] == currVal):
             return True
 
     return False
 
-def flipPieces(x, y):
+
+def flipPieces(x, y, board):
     valToCheck = 1
     currVal = 2
 
@@ -104,10 +161,10 @@ def flipPieces(x, y):
         newX += xDir
         newY += yDir
         
-        if (newX > 7 or newX < 0 or newY > 7 or newY < 0 or boardArr[newY][newX] == currVal):
+        if (newX > 7 or newX < 0 or newY > 7 or newY < 0 or board[newY][newX] == currVal):
             continue
         
-        while (boardArr[newY][newX] == valToCheck):
+        while (board[newY][newX] == valToCheck):
             newX += xDir
             newY += yDir
 
@@ -116,12 +173,12 @@ def flipPieces(x, y):
                 newY -= yDir
                 break
 
-        if (boardArr[newY][newX] == currVal):
+        if (board[newY][newX] == currVal):
             newX -= xDir
             newY -= yDir
             print(f"{newX},{newY}")
-            while (boardArr[newY][newX] == valToCheck):
-                boardArr[newY][newX] = currVal
+            while (board[newY][newX] == valToCheck):
+                board[newY][newX] = currVal
                 newX -= xDir
                 newY -= yDir
                 if (newX > 7 or newX < 0 or newY > 7 or newY < 0):
@@ -129,7 +186,11 @@ def flipPieces(x, y):
                 print(f"{newX},{newY}")
 
 
-def validMoves(count):
+def validMoves(count, board):
+
+    if (count >= 2):
+        displayVictor()
+
     drawBoard()
 
     global valid
@@ -139,28 +200,28 @@ def validMoves(count):
 
     for x in range(8):
         for y in range(8):
-            if (checkPos(x, y)):
+            if (checkPos(x, y, board)):
                 placePiece(x, y, "yellow")
                 valid.append([x,y])
     print(valid)
 
-    if (turn == "black" and valid == [] and count < 2):
-        turn = "white"
-        validMoves(count + 1)
+    if (turn == user and valid == [] and count < 2):
+        turn = cpu
+        cpuMove(count + 1, board)
 
     elif (valid == [] and count < 2):
         turn = "black"
-        validMoves(count + 1)
+        validMoves(count + 1, board)
 
 
 
-def cpuMove():
+def cpuMove(count, board):
     global turn
     x = -1
     y = -1
     maxHeur = -11
 
-    validMoves(0)
+    validMoves(count, board)
 
     for [tempx,tempy] in valid:
         if (boardHeur[tempy][tempx] > maxHeur):
@@ -168,12 +229,14 @@ def cpuMove():
             y = tempy
             maxHeur = boardHeur[tempy][tempx]
 
-    if (x, y != -1, -1):
+    if (x != -1 or y != -1):
         if (cpu == "black"):
             boardArr[y][x] = 1
         else:
             boardArr[y][x] = 2
-        flipPieces(x,y)
+        print(f"CPU placed at: {x}, {y}")
+        print()
+        flipPieces(x,y, board)
     turn = user
 
 
@@ -184,7 +247,7 @@ def mouseXY(event):
     x, y = math.floor(event.x / 100), math.floor(event.y / 100)
 
     if (turn == user and [x,y] in valid):
-        flipPieces(x,y)
+        flipPieces(x,y, boardArr)
         turn = cpu
         if (user == "black"):
             boardArr[y][x] = 1
@@ -192,27 +255,27 @@ def mouseXY(event):
             boardArr[y][x] = 2
 
     elif (not(cpuOn) and turn == cpu and [x,y] in valid):
-        flipPieces(x,y)
+        flipPieces(x,y, boardArr)
         turn = user
         if (cpu == "black"):
             boardArr[y][x] = 1
         else:
             boardArr[y][x] = 2
     
-    print(f"Piece placed at: {x}, {y}")
+    print(f"User placed at: {x}, {y}")
     print()
 
     if (cpuOn and turn == cpu):
-        cpuMove()
-        print(f"Piece placed at: {x}, {y}")
-        print()
+        cpuMove(0, boardArr)
 
-    validMoves(0)
+    validMoves(0, boardArr)
 
 
-validMoves(0)
 
 
-canvas.pack()
+
+validMoves(0, boardArr)
+
+canvas.pack(side="left")
 window.bind("<Button-1>", mouseXY)
 window.mainloop()
